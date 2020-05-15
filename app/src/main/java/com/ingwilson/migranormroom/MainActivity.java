256edfbc3321cafb76a02dbbd8bc992ec3afdfc4
Wilson Callisaya <demarktec@gmail.com>
2020-05-14 20:26:38 -0500

    Añadiendo recyclerView con un drawable personalizado, con notifydatasetchanged, y envio de parametros desde el recyclerview a updatecontact

--- app/src/main/java/com/ingwilson/migranormroom/MainActivity.java
+++ app/src/main/java/com/ingwilson/migranormroom/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ingwilson.migranormroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +11,17 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton mAddContactFloatingActionButton;
     private static final int RC_CREATE_CONTACT = 1;
     private static final int RC_UPDATE_CONTACT = 2;
+
+    private RecyclerView mContactsRecyclerView;
+    private AdapterContacto mContactRecyclerAdapter;
+    private List<Contacto> misdatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, RC_CREATE_CONTACT);
             }
         });
+        mContactsRecyclerView = findViewById(R.id.contactsRecyclerView);
+        mContactsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        int colors[] = {ContextCompat.getColor(this, R.color.colorAccent),
+                ContextCompat.getColor(this, android.R.color.holo_red_light),
+                ContextCompat.getColor(this, android.R.color.holo_orange_light),
+                ContextCompat.getColor(this, android.R.color.holo_green_light),
+                ContextCompat.getColor(this, android.R.color.holo_blue_dark),
+                ContextCompat.getColor(this, android.R.color.holo_purple)};
+        mContactRecyclerAdapter = new AdapterContacto(this, new ArrayList<Contacto>(), colors);
+        mContactsRecyclerView.setAdapter(mContactRecyclerAdapter);
+        loadContacts();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -33,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
         } else if (requestCode == RC_UPDATE_CONTACT && resultCode == RESULT_OK) {
 // loadContacts();
         }
+    }
+    private void loadContacts() {
+        misdatos = new ArrayList<>();
+        misdatos.add( new Contacto("JUAN", "Ticona", "952002434"));
+        misdatos.add( new Contacto("WILSON", "Maquera", "954350345"));
+        misdatos.add(new Contacto("ADRIAN", "Mamani", "943545453"));
+        misdatos.add( new Contacto("CARLA", "Fuentes", "954543534"));
+        misdatos.add(new Contacto("Delia", "Caceres", "969558585"));
+        mContactRecyclerAdapter.updateData(misdatos);
     }
 }
